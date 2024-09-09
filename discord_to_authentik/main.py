39b83e4987ae7b1510c5c3115614d36db0b5da05
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-09-09 15:38:00 +0200

    feature(auto_role): Synchronize roles to authentik users

--- discord_to_authentik/main.py
+++ discord_to_authentik/main.py
@@ -15,6 +15,10 @@ OWNER_ID = [int(os.environ["DISCORD_OWNER_ID"])]
 AUTHENTIK_BEARER_TOKEN = os.environ["AUTHENTIK_TOKEN"]
 AUTHENTIK_HOST = os.environ["AUTHENTIK_HOST"]
 DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
+DISCORD_AUTO_SYNC_CREATE_DELETE = os.environ.get(
+    "DISCORD_AUTO_SYNC_CREATE_DELETE", False
+)
+DISCORD_AUTO_SYNC_ROLES = os.environ.get("DISCORD_AUTO_SYNC_ROLES", False)
 
 
 class MainCog(commands.Cog):
@@ -33,7 +37,7 @@ class MainCog(commands.Cog):
         for command in commands:
             await ctx.reply(f"Synced {command.name} command group", ephemeral=True)
 
-    @commands.is_owner()    # type: ignore
+    @commands.is_owner()  # type: ignore
     @app_commands.command(
         name="sync_roles", description="Sync roles from discord to authentik"
     )
@@ -68,7 +72,75 @@ class MainCog(commands.Cog):
                 core_api.core_groups_destroy(group.pk)
                 deleted_roles.append(group.name)
 
-        await ctx.response.send_message(f"Synced roles: {"\n".join(synced_roles)} | Deleted roles: {"\n".join(deleted_roles)}", ephemeral=True)
+        await ctx.response.send_message(
+            f"Synced roles: {"\n".join(synced_roles)} | Deleted roles: {"\n".join(deleted_roles)}",
+            ephemeral=True,
+        )
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        if not DISCORD_AUTO_SYNC_ROLES:
+            return
+
+        core_api = authentik_client.CoreApi(self.authentik_client)
+        assert after.guild is not None
+        found_user = core_api.core_users_list(username=after.name).results
+
+        if found_user is None:
+            LOGGER.warning(f"User {after.name} not found in authentik. Sync manually!")
+            return
+
+        user_account_request = authentik_client.models.UserAccountRequest(
+            pk=found_user[0].pk,
+        )
+
+        for role in after.roles:
+            if role.is_bot_managed():
+                continue
+            if role.is_default():
+                continue
+
+            found_group = core_api.core_groups_list(name=role.name).results
+            if not found_group:
+                LOGGER.warning(
+                    f"Group {role.name} not found in authentik. Sync manually!"
+                )
+                continue
+
+            if (
+                found_group[0].users is not None
+                and found_user[0].pk in found_group[0].users
+            ):
+                LOGGER.debug(f"User {after.name} already in group {role.name}")
+                continue
+
+            if after.name in []:
+                continue
+
+            try:
+                core_api.core_groups_add_user_create(
+                    found_group[0].pk, user_account_request
+                )
+                LOGGER.info(f"Added user {after.name} to group {role.name}")
+            except Exception as e:
+                LOGGER.error(f"Error adding user to group: {e}")
+                continue
+
+        for group in core_api.core_groups_list().results:
+            removed_roles = list(set(before.roles) - set(after.roles))
+            if group.name not in [role.name for role in removed_roles]:
+                continue
+
+            if (
+                group.attributes is None
+                or group.attributes.get("discord_role_id") is None
+            ):
+                continue
+
+            removed_role = after.guild.get_role(group.attributes["discord_role_id"])
+            assert removed_role is not None
+            core_api.core_groups_remove_user_create(group.pk, user_account_request)
+            LOGGER.info(f"Removed user {after.name} from group {removed_role.name}")
 
 
 class AuthentikSync(commands.Bot):
@@ -76,6 +148,7 @@ class AuthentikSync(commands.Bot):
         intents = Intents.default()
         intents.messages = True
         intents.message_content = True
+        intents.members = True
         self.authentik_client = kwargs.get("authentik_client")
         self.owner_id = OWNER_ID
         super().__init__(*args, **kwargs, intents=intents)
